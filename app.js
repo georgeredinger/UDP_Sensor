c132083e6193c31a91f001393ed1fb9ba51196bf
George Redinger <george@georgeredinger.com>
2013-05-24 16:23:41 -0700

    write received udp packet bodies to console

---
+++ app.js
@@ -0,0 +1,19 @@
+var PORT = 12345;
+var HOST = '0.0.0.0';
+
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+server.on('listening', function () {
+        var address = server.address();
+            console.log('UDP Server listening on ' + address.address + ":" + address.port);
+            
+});
+
+server.on('message', function (message, remote) {
+        console.log(remote.address + ':' + remote.port +' - ' + message);
+
+});
+
+server.bind(PORT, HOST);
+
+
