1fea7793142dc4069b4326ff1e8ebd083916f610
George Redinger <george@georgeredinger.com>
2013-05-24 16:49:51 -0700

    UDP data making it to browser console

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
 var PORT = 12345;
 var HOST = '0.0.0.0';
-
 var dgram = require('dgram');
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(8080);
 var server = dgram.createSocket('udp4');
 server.on('listening', function () {
         var address = server.address();
             console.log('UDP Server listening on ' + address.address + ":" + address.port);
-            
 });
 
-server.on('message', function (message, remote) {
-        console.log(remote.address + ':' + remote.port +' - ' + message);
 
-});
 
 server.bind(PORT, HOST);
 
+
+function handler (req, res) {
+    fs.readFile(__dirname + '/index.html',
+                function (err, data) {
+                    if (err) {
+                        res.writeHead(500);
+                        return res.end('Error loading index.html');
+                    }
+
+                    res.writeHead(200);
+                    res.end(data);
+                });
+}
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+    console.log(data);
+    });
+    server.on('message', function (message, remote) {
+          console.log(remote.address + ':' + remote.port +' - ' + message);
+          socket.emit('accel', { xy: message });
+
+    });
+
+});
 
