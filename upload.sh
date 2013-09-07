056595dbdfee9d61bee0dacd68610a2156a6a7b0
George Redinger <george@georgeredinger.com>
2013-09-07 11:44:19 -0700

    erase option on avrdude is mandatory (-e)

--- upload.sh
+++ upload.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-what about the -e erase option?
-
-avrdude -c usbtiny -p m168p -D -Uflash:w:./.build/atmega168/firmware.hex
+avrdude -c usbtiny -p m168p -e -D -Uflash:w:./.build/atmega168/firmware.hex
